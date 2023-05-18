1a7c13daa394e65de442ed8bf5785d7fce0bc2b7
Nikita Mikhailov <kdevaulo@gmail.com>
2023-05-19 00:04:39 +0500

    MagicLock added to gameplay

--- Assets/Kdevaulo/LocksTest/Scripts/LockSystem/LocksContainer.cs
+++ Assets/Kdevaulo/LocksTest/Scripts/LockSystem/LocksContainer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using Kdevaulo.LocksTest.Scripts.LockSystem.ClickLockBehaviour;
 using Kdevaulo.LocksTest.Scripts.LockSystem.CylinderLockBehaviour;
+using Kdevaulo.LocksTest.Scripts.LockSystem.MagicLockBehaviour;
 using Kdevaulo.LocksTest.Scripts.LockSystem.MoveObjectLockBehaviour;
 using Kdevaulo.LocksTest.Scripts.LockSystem.ResistanceClickLockBehaviour;
 using Kdevaulo.LocksTest.Scripts.LockSystem.RotateImageLockBehaviour;
@@ -24,7 +25,8 @@ namespace Kdevaulo.LocksTest.Scripts.LockSystem
                 {typeof(CylinderLockView), typeof(CylinderLock)},
                 {typeof(MoveObjectLockView), typeof(MoveObjectLock)},
                 {typeof(ResistanceClickLockView), typeof(ResistanceClickLock)},
-                {typeof(RotateImageLockView), typeof(RotateImageLock)}
+                {typeof(RotateImageLockView), typeof(RotateImageLock)},
+                {typeof(MagicLockView), typeof(MagicLock)}
             };
 
             foreach (var pair in _locksWithViews)
